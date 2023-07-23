cbb27948d9af79171cf3f7ac279f95140e847c1f
priyashanmugaraj <priyashanmugaraj95@gmail.com>
2023-07-23 16:32:03 +0530

    implenting parcel

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import React from 'react'
+import ReactDOM  from 'react-dom/client';
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a H1 Tag"),
+    React.createElement("h1", {}, "I am a H8 Tag"),
     React.createElement("h2", {}, "I am a H2 Tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am a H1 Tag"),
-    React.createElement("h2", {}, "I am a H2 Tag"),
+    React.createElement("h2", {}, "I am a H8 Tag"),
   ]),
 ]);
 console.log(heading);
